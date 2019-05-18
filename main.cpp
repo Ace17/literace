782f7c8c184867295e938adbd9eb0a6ce687af0f
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2019-05-18 16:56:53 +0200

    Implement directions

--- main.cpp
+++ main.cpp
@@ -2,18 +2,26 @@
 #include <cassert>
 #include "SDL.h"
 
+auto const WIDTH = 800;
+auto const HEIGHT = 600;
+
 int x = 128;
 int y = 128;
 
-bool left, right, up, down;
+struct Input
+{
+  bool quit;
+  bool left, right, up, down;
+  bool boost;
+};
 
-bool processInput()
+void processInput(Input& input)
 {
   SDL_Event event;
   while(SDL_PollEvent(&event))
   {
     if(event.type == SDL_QUIT)
-      return false;
+      input.quit = true;
 
     if(event.type == SDL_KEYDOWN || event.type == SDL_KEYUP)
     {
@@ -21,36 +29,89 @@ bool processInput()
       switch(event.key.keysym.scancode)
       {
       case SDL_SCANCODE_ESCAPE:
-        return false;
+        input.quit = true;
+      case SDL_SCANCODE_LSHIFT:
+        input.boost = isPressed;
+        break;
       case SDL_SCANCODE_LEFT:
-        left = isPressed;
+        input.left = isPressed;
         break;
       case SDL_SCANCODE_RIGHT:
-        right = isPressed;
+        input.right = isPressed;
         break;
       case SDL_SCANCODE_UP:
-        up = isPressed;
+        input.up = isPressed;
         break;
       case SDL_SCANCODE_DOWN:
-        down = isPressed;
+        input.down = isPressed;
         break;
       }
     }
   }
-
-  return true;
 }
 
-void updateGame()
+enum class Direction
 {
-  if(left)
-    --x;
-  if(right)
-    ++x;
-  if(up)
-    --y;
-  if(down)
-    ++y;
+  Idle,
+  Left,
+  Down,
+  Right,
+  Up,
+};
+
+static const int dirs[][2] =
+{
+  {0, 0},
+  {-1, 0},
+  {0, 1},
+  {1, 0},
+  {0, -1},
+};
+
+bool isOpposed(Direction a, Direction b)
+{
+  int a_dx = dirs[(int)a][0];
+  int a_dy = dirs[(int)a][1];
+  int b_dx = dirs[(int)b][0];
+  int b_dy = dirs[(int)b][1];
+
+  return a_dx == -b_dx && a_dy == -b_dy;
+}
+
+struct Bike
+{
+  int x, y;
+  Direction direction;
+};
+
+Bike g_bike;
+
+void updateGame(Input input)
+{
+  Direction wantedDirection = g_bike.direction;
+
+  if(input.left)
+    wantedDirection = Direction::Left;
+  if(input.right)
+    wantedDirection = Direction::Right;
+  if(input.up)
+    wantedDirection = Direction::Up;
+  if(input.down)
+    wantedDirection = Direction::Down;
+
+  if(!isOpposed(g_bike.direction, wantedDirection))
+    g_bike.direction = wantedDirection;
+
+  int speed = 1;
+
+  if(input.boost)
+    speed = 2;
+
+  x += dirs[(int)g_bike.direction][0] * speed;
+  y += dirs[(int)g_bike.direction][1] * speed;
+
+  x = (x + WIDTH)%WIDTH;
+  y = (y + HEIGHT)%HEIGHT;
 }
 
 void drawScreen(SDL_Renderer* renderer)
@@ -73,16 +134,22 @@ int main()
 {
   SDL_Init(SDL_INIT_EVERYTHING);
 
-  auto window = SDL_CreateWindow("Literace", 0, 0, 800, 600, 0);
+  auto window = SDL_CreateWindow("Literace", 0, 0, WIDTH, HEIGHT, 0);
   assert(window);
 
   /* We must call SDL_CreateRenderer in order for draw calls to affect this window. */
   auto renderer = SDL_CreateRenderer(window, -1, 0);
   assert(renderer);
 
-  while(processInput())
+  Input input{};
+
+  while(1)
   {
-    updateGame();
+    processInput(input);
+    if(input.quit)
+      break;
+
+    updateGame(input);
     drawScreen(renderer);
     SDL_Delay(10);
   }
